ffd68fabdd761be66bc5e7e45508c26c78b7e071
MichaelBel12 <misha84921@gmail.com>
2025-12-15 20:35:03 +0200

    third

--- src/main/java/memory/SharedVector.java
+++ src/main/java/memory/SharedVector.java
@@ -10,46 +10,83 @@ public class SharedVector {
 
     public SharedVector(double[] vector, VectorOrientation orientation) {
         // TODO: store vector data and its orientation
+        this.orientation=orientation;
+        this.vector=vector;
     }
 
     public double get(int index) {
         // TODO: return element at index (read-locked)
-        return 0;
+        if(index<0 || index>=vector.length)
+            throw new IndexOutOfBoundsException("[Sharedvector:Get]: Index out of bounds");
+        readLock();
+        try {
+            double output = vector[index];
+            return output;
+        }
+        finally {
+            readUnlock();
+        }
     }
 
     public int length() {
-        // TODO: return vector length
-        return 0;
+        return vector.length;
     }
 
     public VectorOrientation getOrientation() {
-        // TODO: return vector orientation
-        return null;
+        readLock();
+        try{
+            VectorOrientation orient=orientation;
+            return orient;
+        }
+        finally{readUnlock();}
     }
 
     public void writeLock() {
-        // TODO: acquire write lock
+        lock.writeLock().lock();
     }
 
     public void writeUnlock() {
-        // TODO: release write lock
+        lock.writeLock().unlock();
     }
 
     public void readLock() {
-        // TODO: acquire read lock
+        lock.readLock().lock();
     }
 
     public void readUnlock() {
-        // TODO: release read lock
+        lock.readLock().unlock();
     }
 
     public void transpose() {
-        // TODO: transpose vector
+        writeLock();
+        try{
+            VectorOrientation opposite = (this.orientation == VectorOrientation.ROW_MAJOR) ? VectorOrientation.COLUMN_MAJOR : VectorOrientation.ROW_MAJOR;      
+            this.orientation=opposite;
+        }
+        
+        finally{writeUnlock();}
+        
+        
     }
 
     public void add(SharedVector other) {
-        // TODO: add two vectors
-    }
+        if(vector.length!=other.vector.length)
+            throw new IllegalArgumentException("[Add]: Cannot add vectors with diferent sizes");
+        
+        writeLock();
+        other.readLock();
+        try{
+            if (orientation!=other.orientation)
+             throw new IllegalArgumentException("[Add]: Cannot add vectors with diferent orientations");
+        for (int i=0;i<vector.length;i++){
+            vector[i]+=other.vector[i];
+            }  
+        }
+        finally{
+            writeUnlock();
+            other.readUnlock();
+        }
+    } 
 
     public void negate() {
         // TODO: negate vector
